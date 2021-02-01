1e017eba402a0e2bef59f848543e23f84b86f116
Kyle Maas <kylemaasdev@gmail.com>
2021-02-01 09:04:06 -0500

    Add WebSockets support by default, with optional configuration

--- config.js
+++ config.js
@@ -15,6 +15,10 @@ const config = Config('ssb', {
   connections: {
     incoming: {
       net: [{scope: 'public', transform: 'shs', port: 8008, host: '0.0.0.0'}],
+      // Enable this to add WebSockets support over HTTP.
+      // ws: [{scope: 'public', transform: 'shs', port: 8009, host: '0.0.0.0', http: true}],
+      // Enable this (and customize) to add WebSockets support over HTTPS.
+      // ws: [{scope: 'public', transform: 'shs', port: 8009, host: '0.0.0.0', external: ["example.com"], key: "/etc/letsencrypt/live/example.com/privkey.pem", cert: "/etc/letsencrypt/live/example.com/cert.pem" }],
     },
     outgoing: {
       net: [{transform: 'shs'}],
